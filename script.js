1eb5bbdceade97f391c2977d3da9e4980e4f4fb1
Pivanta <bas677@icloud.com>
2021-01-27 00:12:06 +0000

    get elements, set values, roll dice on click

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-'use strict';
+"use strict";
+
+// Get elements
+var playerOneEl = document.getElementById("score--0");
+var playerTwoEl = document.getElementById("score--1");
+var getDiceEl = document.getElementById("image");
+
+// Set values
+var playerOneScore = (playerOneEl.innerText = 0);
+var playerTwoScore = (playerTwoEl.innerText = 0);
+
+// Get dice images on roll dice click
+const getDiceImages = (dice) => {
+  getDiceEl.src = "dice-" + dice + ".png";
+};
+
+// Roll dice
+document.addEventListener("click", function () {
+  var getRandomNumber = Math.floor(Math.random() * 6 + 1);
+  getDiceImages(getRandomNumber);
+});
