600b60bcbeb3692e87a75313f19903a768266bc8
Pivanta <bas677@icloud.com>
2021-01-28 00:55:40 +0000

    Player one flow complete

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
 
-var playerOneScore = 0;
-var playerTwoScore = 0;
+var p1TotalScore = 0;
+var p2TotalScore = 0;
+var p1CurrentScore = 0;
+var p2CurrentScore = 0;
 
 
 // Get elements
@@ -11,16 +13,21 @@ var playerOneCurrentEl = document.getElementById('current--0');
 var playerTwoCurrentEl = document.getElementById('current--1')
 var getDiceEl = document.getElementById("image");
 var getBtnClassEl = document.getElementsByClassName('btn');
+var getActivePlayerEl = document.getElementsByClassName('player');
 
 // Set default values
-playerOneEl.innerText = playerOneScore;
-playerTwoEl.innerText = playerTwoScore;
+playerOneEl.innerText = p1TotalScore;
+playerTwoEl.innerText = p2TotalScore;
 
 // Get dice images when roll dice btn clicked
 const getDiceImages = (dice) => {
   getDiceEl.src = "dice-" + dice + ".png";
-  updateP1Score(dice);
-  //updateP2Score(dice)
+  if (dice == 1) {
+    switchPlayer();
+    diceRollsOne();
+  } else {
+    updateP1Score(dice);
+  }
 };
 
 // Roll dice on btn click
@@ -31,18 +38,35 @@ getBtnClassEl[1].addEventListener("click", function () {
 
 // Update player one score
 const updateP1Score = (p1score) => {
-  playerOneScore += p1score;
-  playerOneCurrentEl.innerText = playerOneScore;
+  p1CurrentScore += p1score;
+  playerOneCurrentEl.innerText = p1CurrentScore;
 };
 
 // Update player two score
 const updateP2Score = (p2score) => {
-  playerTwoScore += p2score;
-  playerTwoCurrentEl.innerText = playerTwoScore;
+  p2TotalScore += p2score;
+  playerTwoCurrentEl.innerText = p2TotalScore;
 };
 
 // Add score when hold btn clicked
 getBtnClassEl[2].addEventListener('click', function () {
-  playerOneEl.innerText = playerOneScore;
-})
+  p1TotalScore += p1CurrentScore;
+  playerOneEl.innerText = p1TotalScore;
+  playerOneCurrentEl.innerText = 0;
+  switchPlayer()
 
+});
+
+// Switch player
+const switchPlayer = () => {
+getActivePlayerEl[0].classList.toggle('player--active');
+getActivePlayerEl[1].classList.toggle('player--active');
+}
+
+// Reset scores when dice rolls one 
+const diceRollsOne = () => {
+  p1CurrentScore = 0;
+  playerOneCurrentEl.innerText = p1CurrentScore;
+  p1TotalScore = 0;
+  playerOneEl.innerText = p1TotalScore;
+} 
